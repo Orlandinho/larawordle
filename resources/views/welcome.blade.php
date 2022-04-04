1c2df0b31c6d2f4b84442cfb3b503c4c1062fded
Orlandinho <horlandomagic@gmail.com>
2022-04-04 13:14:34 -0300

    Style refactoring

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,9 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Wordle</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="{{ asset("css/app.css") }}">
         <script src="{{ asset("js/app.js") }}" defer></script>
     </head>
@@ -12,9 +15,12 @@
         <main x-data="game"
              @keyup.window="onKeyPress($event.key)"
         >
+            <h1 aria-label="Tony Wordle">
+                <img src="{{ asset('storage/images/tony.png') }}" alt="logo">
+            </h1>
             <div id="game">
-                <template x-for="row in board">
-                    <div class="row">
+                <template x-for="(row, index) in board">
+                    <div class="row" :class="{ 'current' : currentRowIndex === index }">
                         <template x-for="tile in row">
                             <div class="tile" :class="tile.status" x-text="tile.letter"></div>
                         </template>
